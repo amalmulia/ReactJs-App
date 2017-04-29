489aa71018c4c5e8b23d1c7d97ea6c18594c7980
Amal Mulia <amalmulia.pcmaax@yahoo.com>
2017-04-29 11:39:50 +0800

    Starting React

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Leaderboard extends React.Component {
+	render() {
+		return <h1>leaderboard</h1>
+	}
+}
+
+
+ReactDOM.render(
+  <Leaderboard />,
+  document.getElementById('root')
+);
