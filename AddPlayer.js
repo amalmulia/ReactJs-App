9315c931f214744bbbe9728bb526125a15dd6b27
Amal Mulia <amalmulia.pcmaax@yahoo.com>
2017-04-30 00:19:01 +0800

    error addPlayer

---
+++ AddPlayer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class AddPLayer extends React.Component {
+	render() {
+		return (
+			<div>
+				<input type="text" placeholder="Ketik Nama Leaderboard" />
+				<button>Add Player</button>
+			</div>
+		)
+	}
+}
+
+
+export default AddPLayer
--- Leaderboard.js
+++ Leaderboard.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 import Players from './Players.js'
 
+import AddPlayer from './AddPlayer.js'
+
 class Leaderboard extends React.Component {
-	render() {
+	constructor(props) {
+		super(props)
+
 		const PLAYERS = [
 			{id: 1, name: "Amal Mulia", score: 95},
 			{id: 2, name: "Andi Rahma", score: 80},
@@ -11,6 +15,11 @@ class Leaderboard extends React.Component {
 			{id: 4, name: "Bahagia", score: 94}
 		]
 
+		this.state = {
+			members: PLAYERS
+		}
+	}
+	render() {
 		const styles = {
 			container: {
 				padding: 60,
@@ -22,7 +31,8 @@ class Leaderboard extends React.Component {
 	 	return (
 			<div style={styles.container}>
 				<h1>leaderboard</h1>
-				<Players members={PLAYERS}/>
+				<Players members={this.state.members}/>
+				<AddPLayer />
 			</div>
 		)
 	}
