a42f529b3df608a09aafe1404ae960662e6b9fe6
Amal Mulia <amalmulia.pcmaax@yahoo.com>
2017-04-29 23:07:00 +0800

    Styling React inline

--- Leaderboard.js
+++ Leaderboard.js
@@ -7,12 +7,20 @@ class Leaderboard extends React.Component {
 		const PLAYERS = [
 			{id: 1, name: "Amal Mulia", score: 95},
 			{id: 2, name: "Andi Rahma", score: 80},
-			{id: 3, name: "Dinda", score: 100},
+			{id: 3, name: "Dinda", score: 99},
 			{id: 4, name: "Bahagia", score: 94}
 		]
 
+		const styles = {
+			container: {
+				padding: 60,
+				width: 600,
+				margin: 'auto'
+			}
+		}
+
 	 	return (
-			<div>
+			<div style={styles.container}>
 				<h1>leaderboard</h1>
 				<Players members={PLAYERS}/>
 			</div>
--- Player.js
+++ Player.js
@@ -3,16 +3,34 @@ import React from 'react'
 
 class Player extends React.Component {
 	render() {
+		const styles = {
+			player: {
+				display: 'flex',
+				padding: 10
+			},
+			name: {
+				flex: '1'
+			},
+			playerScore: {
+				width: 200
+			},
+			score: {
+				padding: 20
+			},
+			button: {
+				padding: 10
+			}
+		}
 		return  (
-			<div>
-				<div>
+			<div style={styles.player}>
+				<div style={styles.name}>
 					{this.props.name}
 				</div>
-				<div>
+				<div style={styles.playerScore}>
 					<div>
-						<button>-</button>
-						<span>{this.props.score}</span>
-						<button>+</button>
+						<button style={styles.button}>-</button>
+						<span style={styles.score}>{this.props.score}</span>
+						<button style={styles.button}>+</button>
 					</div>
 				</div>
 			</div>
