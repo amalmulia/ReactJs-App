794bed73be2e187863d3f041e44e53ae32ddeead
Amal Mulia <amalmulia.pcmaax@yahoo.com>
2017-05-01 22:34:41 +0800

    Final Project

--- Leaderboard.js
+++ Leaderboard.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import Players from './Players.js'
-
 import AddPlayer from './AddPlayer.js'
 
 /*global AddPLayer*/
@@ -21,14 +20,26 @@ class Leaderboard extends React.Component {
 			{id: 1, name: "Amal Mulia", score: 95},
 			{id: 2, name: "Andi Rahma", score: 80},
 			{id: 3, name: "Dinda", score: 99},
-			{id: 4, name: "Bahagia", score: 94}
+			{id: 4, name: "Ade", score: 94}
 		]
 
 		this.state = {
 			members: PLAYERS
 		}
+		this.onPlayerAdd = this.onPlayerAdd.bind(this)
 	}
+	
+	onPlayerAdd(name) {
+		let new_members = this.state.members
+		new_members.push({id: new_members.length + 1, name: name, score: 10})
+
+		this.setState({
+			members: new_members
+		}) 
+	}
+	
 	render() {
+
 		const styles = {
 			container: {
 				padding: 60,
@@ -41,7 +52,7 @@ class Leaderboard extends React.Component {
 			<div style={styles.container}>
 				<h1>leaderboard</h1>
 				<Players members={this.state.members} />
-				<AddPlayer />
+				<AddPlayer onAdd={this.onPlayerAdd.bind(this)}/>
 			</div>
 		)
 	}
